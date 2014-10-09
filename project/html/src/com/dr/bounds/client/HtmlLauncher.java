e8bace2eb9d0b11d326d94673c900fef2fa8b30b
DanielRudn <danielrudn96@hotmail.com>
2014-10-09 14:45:08 -0400

    Initial Commit: CURRENT FEATURES: * Working (kind of, invite problems) google play games services * Bad map generator TODO: * Fix invite bugs * Fix player movement and multiplayer movement
    Signed-off-by: DanielRudn <danielrudn96@hotmail.com>

---
+++ project/html/src/com/dr/bounds/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.dr.bounds.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.dr.bounds.MainGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new MainGame();
+        }
+}
