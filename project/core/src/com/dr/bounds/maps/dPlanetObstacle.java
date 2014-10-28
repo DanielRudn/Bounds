6ceaccd50f736e9ac618c1c7119fe187f5436c77
DanielRudn <danielrudn96@hotmail.com>
2014-10-28 00:19:42 -0400

    * Created and implemented an abstract MapType class  -> New bug occured where one player does not always sync on restarting matches * Began work on SpaceMapType
    Signed-off-by: DanielRudn <danielrudn96@hotmail.com>

--- project/core/src/com/dr/bounds/maps/dPlanetObstacle.java
+++ project/core/src/com/dr/bounds/maps/dPlanetObstacle.java
@@ -1,10 +1,11 @@
 package com.dr.bounds.maps;
 
+import java.util.Random;
+
 import com.DR.dLib.dImage;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.dr.bounds.Player;
 
@@ -16,11 +17,29 @@ public class dPlanetObstacle extends dObstacle {
 	private dImage moonImage;
 	// the x value for the moon rotation
 	private float rotationTime = 0;
-	private float rotationDuration = 2f;
+	// the offset from start position of the angle for the moon
+	private float angleOffset = 0;
+	// rotation speed of the moon
+	private float rotationSpeed = 1f;
+	// minimum rotation speed
+	private final float MIN_ROTATION_SPEED = 0.5f;
+	// the distance between the planet and the moon
+	private int moonDistance = 32;
+	// the rng to determine the random numbers, gotten from constructor in order to match seed with other players
+	private Random rng;
+	// colors for planet and moon
+	Color[] planetColors = new Color[]{new Color(46f/256f, 204f/256f, 113f/256f,1f), // green
+			new Color(52f/256f, 152f/256f, 219f/256f,1f), // blue
+			new Color(231f/256f, 76f/256f, 60f/256f,1f), // red
+			new Color(241f/256f, 196f/256f, 15f/256f,1f), //yellow
+			new Color(243f/256f, 156f/256f, 18f/256f,1f)}; // orange
 	
-	public dPlanetObstacle(float x, float y, Texture texture, Player p) {
+	public dPlanetObstacle(float x, float y, Texture texture, Player p, Random random) {
 		super(x, y, texture, p);
-		int moonProbability = MathUtils.random(3);
+		rng = random;
+		int colorIndex = rng.nextInt(planetColors.length);
+		setColor(planetColors[colorIndex]);
+		int moonProbability = rng.nextInt(3);
 		moonProbability = 2;
 		if(moonProbability == 0 || moonProbability == 1)
 		{
@@ -31,8 +50,18 @@ public class dPlanetObstacle extends dObstacle {
 			hasMoon = true;
 			moonImage = new dImage(0,0,texture);
 			moonImage.setDimensions(64f, 64f);
-			moonImage.setColor(Color.BLACK);
+			try{
+			moonImage.setColor(planetColors[colorIndex+1]);
+			}
+			catch(Exception e)
+			{
+				moonImage.setColor(planetColors[0]);
+			}
 			moonImage.setPos(getX() - 15 - moonImage.getWidth(), getY() - 15 - moonImage.getHeight());
+			angleOffset = rng.nextInt(2);
+			rotationTime = angleOffset;
+			rotationSpeed = MIN_ROTATION_SPEED + rng.nextFloat();
+			moonDistance = rng.nextInt(64);
 		}
 	}
 	
@@ -54,9 +83,9 @@ public class dPlanetObstacle extends dObstacle {
 		{
 			if(rotationTime < 2 * Math.PI)
 			{
-				moonImage.setPos((float) (getX() + getWidth()/2f + Math.cos(rotationTime) * (moonImage.getWidth() + getWidth()/2f)),
-						(float)(getY() + getHeight()/2f + Math.sin(rotationTime) * (moonImage.getWidth() + getHeight()/2f + 32)));
-				rotationTime+=delta;
+				moonImage.setPos((float) (getX() + getWidth()/2f + Math.cos(rotationTime) * (moonImage.getWidth() + getWidth()/2f + moonDistance)),
+						(float)(getY() + getHeight()/2f + Math.sin(rotationTime) * (moonImage.getWidth() + getHeight()/2f + moonDistance)));
+				rotationTime+=delta*rotationSpeed;
 			}
 			else if(rotationTime >= 2 * Math.PI)
 			{
